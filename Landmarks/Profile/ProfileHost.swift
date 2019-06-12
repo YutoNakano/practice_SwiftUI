f8d8bfebb2468bfd75f485040d8cee9186d41b7e
中野湧仁 <nakanoyuuhitoshi@nakanoyuuhitoshinoMacBook-puro.local>
2019-06-12 10:30:54 +0900

    feat: present ProfileHostView

--- Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Profile/ProfileHost.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 
 struct ProfileHost : View {
+    @State var profile = Profile.default
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello World!")
     }
 }
 
