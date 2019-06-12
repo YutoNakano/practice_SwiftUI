d5292fe96898739cca57852793d646efff37880b
中野湧仁 <nakanoyuuhitoshi@nakanoyuuhitoshinoMacBook-puro.local>
2019-06-12 10:18:37 +0900

    initial commit

---
+++ Landmarks/Supporting Views/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by 中野湧仁 on 2019/06/12.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct ProfileHost_Previews : PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
+#endif
