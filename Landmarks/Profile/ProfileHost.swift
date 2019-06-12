5e5aae39d14624def788b1021ce6026837bb532b
中野湧仁 <nakanoyuuhitoshi@nakanoyuuhitoshinoMacBook-puro.local>
2019-06-12 20:36:42 +0900

    Clear:UIControl tutorial

--- Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Profile/ProfileHost.swift
@@ -9,9 +9,34 @@
 import SwiftUI
 
 struct ProfileHost : View {
+    @Environment(\.editMode) var mode
     @State var profile = Profile.default
+    @State var draftProfile = Profile.default
+    
     var body: some View {
-        Text("Hello World!")
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if self.mode?.value == .active {
+                    Button(action: {
+                        self.profile = self.draftProfile
+                        self.mode?.animation().value = .inactive
+                    }) {
+                        Text("Dane")
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            if self.mode?.value == .inactive {
+                ProfileSummary(profile: self.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onDisappear() {
+                        self.draftProfile = self.profile
+                }
+            }
+        }
+        .padding()
     }
 }
 
